b1eb24754958d5cf3937ad7b080fcd06aad41d19
Vuyo <vuyoxhamfu@gmail.com>
2021-12-18 02:16:10 +0200

    cleaning master

--- hackerrank.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo npm install -g n
-sudo n stable
-sudo npm install -g @angular/cli
-npm i
-ng test --browsers=ChromeHeadless --watch=false
--- src/app/pages/profile/duration.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { durationPipe } from './duration.pipe';
-
-describe('ProfilePipe', () => {
-  it('create an instance', () => {
-    const pipe = new durationPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
