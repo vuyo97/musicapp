71bbcfaebbc692db4e209b2efba130d3eb316178
Vuyo <vuyoxhamfu@gmail.com>
2021-12-22 15:38:19 +0200

    UI modifications

--- public/src/app/pages/profile/duration.pipe.ts
+++ public/src/app/pages/profile/duration.pipe.ts
@@ -30,12 +30,12 @@ export class durationPipe implements PipeTransform {
     if(hDisplay){
       var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours ") : "";
     var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes, ") : "";
-    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" :" seconds") : "";
       return hDisplay + mDisplay + sDisplay;
     }else{
 
-      var mDisplay = m > 0 ? m + (m == 1 ? " minute " : ":") : "";
-      var sDisplay = s > 0 ? s + (s == 1 ? " second" : "") : "";
+      var mDisplay = m > 0 ? m + (m == 1 ? "" : ":") : "";
+      var sDisplay = s > 0 ? s + (s == 1 ? "" : "") : "";
         return mDisplay + sDisplay;
 
       }
