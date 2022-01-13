bbb3b7e3d9d16c7b177f4f5496501e552dc5db62
Vuyo <vuyoxhamfu@gmail.com>
2022-01-13 20:10:33 +0200

    updated version

--- public/src/app/pages/home/home.component.ts
+++ public/src/app/pages/home/home.component.ts
@@ -8,15 +8,28 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   imageSrc = '../../../assets/images/audio.png'  ;
- 
-  constructor(private router: Router,private readonly route: ActivatedRoute) { }
+  //currentUser:object;
+
+  constructor(private router: Router,private readonly route: ActivatedRoute) { 
+    
+  }
   isAlbumsPage = false;
   isRadioPage = false;
   isPodPage = false;
   isTracksPage = false;
   
+  
   ngOnInit(): void {
-    console.log(localStorage);
+    console.log(localStorage.userObj);
+    const user = localStorage.getItem('userObj');
+      // if user is null
+      if(!user) {
+        // go for login
+        console.log("Please Log in!");
+      } else {
+        // redirect to home through navigateByURL()
+        console.log("Welcome back"+localStorage.userObj.name );
+      }
   }
 
   // ngAfterContentInit(){
