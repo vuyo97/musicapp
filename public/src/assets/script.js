5a81c49d991cae7e4228a73b7c640281e4259d83
Vuyo <vuyoxhamfu@gmail.com>
2022-01-15 20:20:02 +0200

    updated : player fully functional

--- public/src/assets/script.js
+++ public/src/assets/script.js
@@ -17,19 +17,17 @@ window.dzAsyncInit = function() {
   var e = document.createElement('script');
   e.src = 'https://e-cdns-files.dzcdn.net/js/min/dz.js';
   e.async = true;
- // document.getElementById('dz-root').appendChild(e);
+
  //DZ.logout();
   console.log("logged out, sdk init and ready");;
   }());
 
-  //var loginbtn = document.getElementById('loginbtn');
   function login(){
   localStorage.removeItem('userObj');
   DZ.login(function(response) {
       if (response.authResponse) {
         console.log('Welcome!  Fetching your information.... ');
         DZ.api('/user/me', function(response) {
-        //  loginbtn.value = "logout"
          // DZ.player.playAlbum(285723602,true);
           console.log(response);
           localStorage.setItem('userObj', JSON.stringify(response))
@@ -50,20 +48,6 @@ DZ.getLoginStatus(function(response) {
     DZ.login();
   }
 });
-
-// // DZ.init({
-// //     appId: '514702',
-// //     channelUrl: 'http://localhost:5000/api/deezer/callback',
-// //     player: {
-// //       onload: function() {
-// //         DZ.player.playTracks([654870372], true)
-// //       }
-// //     }
-// //   })
-
-//  DZ.ready(function(sdk_options){
-//      console.log('DZ SDK is ready', sdk_options);
-//  });
 
 // DZ.init({
 //     appId  : '514702',
